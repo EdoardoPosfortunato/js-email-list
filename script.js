f0933ba5e33ed4ed4ccc156c7b838ff40241b875
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-05-11 15:08:59 +0200

    feat: layout e refresh

--- script.js
+++ script.js
@@ -3,34 +3,35 @@ const arrayEmail = [];
 let li = '';
 const listaEmail = document.getElementById("lista");
 const btn = document.getElementById("btn");
-
-
-for (let i = 1; i < 10; i++) {
-
-    axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-
-        .then((resp) => {
-
-            let currEmail = resp.data.response;
-            li += `<li>${currEmail}</li>`
-
-            // console.log(currEmail);
-            arrayEmail.push(currEmail);
-            
-            
-            if (i == 9) {
-                listaEmail.innerHTML = li;
-                console.log(arrayEmail)
-            }
-
-        });
-
-}
+let numeroEmailRichieste = 10
 
 
 btn.addEventListener("click", stampa)
 
 function stampa() {
-    console.log(arrayEmail)
+
+    listaEmail.innerHTML = '';
+    li = ``;
+
+    for (let i = 1; i <= numeroEmailRichieste; i++) {
+
+        axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+            .then((resp) => {
+
+                let currEmail = resp.data.response;
+                li += `<li class="list-group-item">${currEmail}</li>`
+
+                // console.log(currEmail);
+                arrayEmail.push(currEmail);
+
+                if (i == numeroEmailRichieste) {
+                    listaEmail.innerHTML = li;
+                    console.log(arrayEmail)
+                }
+
+            });
+
+    }
 }
